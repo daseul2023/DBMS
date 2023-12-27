3b19bc37912dc4ad93010f8e2a88c655f7f07b61
Daseul Jeong <godyddla@naver.com>
2023-12-27 18:14:15 +0900

    12월 27일

--- Oracle Script/board 테이블.sql
+++ Oracle Script/board 테이블.sql
@@ -7,3 +7,26 @@ desc board;
 
 
 select * from board order by seq desc;
+
+update board 
+set title ='제목 수정됨', write = '이순신', content = '내용 수정됨'
+where seq = 4;
+
+commit;
+
+desc board;
+
+delete board where seq = 3;
+rollback;
+
+update board 
+set cnt = cnt +1 
+where seq = 7;
+
+commit;
+
+desc users;
+
+select * from users 
+where id = 'user' and password = '1234';
+
